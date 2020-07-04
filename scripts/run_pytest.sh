1fa5635d6e291fd46a825667bbdbfc64e8eee159
Dave Korhumel <dpk2442@gmail.com>
2020-07-04 16:49:47 -0700

    change scripts to run under bash

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$(dirname "$0")/.."
 pytest --verbose --cov-config tox.ini --cov-report html --cov-report term --cov pysmith --html test_report.html --self-contained-html tests
